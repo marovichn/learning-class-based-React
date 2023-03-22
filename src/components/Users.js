e57e59ceca7ece79bb67d870856bb13111dc32ad
marovichn <nikolamarovic.11@gmail.com>
2023-03-22 22:44:29 +0100

    error-boundling-learning

--- src/components/Users.js
+++ src/components/Users.js
@@ -11,6 +11,12 @@ class Users extends Component {
     };
   }
 
+  componentDidUpdate() {
+    if (this.props.users.length === 0) {
+      throw new Error("NO users provided!");
+    }
+  }
+
   toggleUsersHandler() {
     this.setState((curState) => {
       return { showUsers: !curState.showUsers };
